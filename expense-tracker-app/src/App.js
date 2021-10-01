739822dc465a57c6bda08bff181644fdc42a1cfc
Alex Wade <galexwadejr@yahoo.com>
2021-10-01 15:52:42 -0500

    added ExpenseItem component and styled

--- expense-tracker-app/src/App.js
+++ expense-tracker-app/src/App.js
@@ -1,8 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <h1>Let's get started!</h1>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
