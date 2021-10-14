c6fdafffd6fa4ee0fcbdf0a27446477974bfb98d
Alex Wade <galexwadejr@yahoo.com>
2021-10-14 16:14:39 -0500

    added starter code and created navbar

---
+++ google-homepage-clone/src/components/Home.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Home = () => {
+  return (
+    <Container>
+      <h1>Home</h1>
+    </Container>
+  );
+};
+
+export default Home;
